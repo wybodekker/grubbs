5b04ebeb1fd8d1901e1bb26f47e8d0dd206d0301
Wybo Dekker <wybo@xs4all.nl>
2013-11-12 17:09:19 +0100

    converted to simple script, with gendoc formatted doc

--- lib/grubbs/version.rb
+++
@@ -1,9 +0,0 @@
-# Sets constant Grubbs::VERSION, the version number of
-# this package. It is used in Gem creation but can also be consulted after
-# require'ing 'grubbs'.
-module Grubbs
-  # The current version of this package
-  VERSION = '1.2'
-end
-
-# $Id$
